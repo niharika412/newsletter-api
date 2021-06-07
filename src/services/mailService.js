4d2876bc51a83ebd0edd0110417527e99fe0ae1c
Niharika Pandit <niharikap219@gmail.com>
2021-06-07 20:38:56 +0530

    enhancement(read-file): Read file and sent it to multiple emails

--- src/services/mailService.js
+++ src/services/mailService.js
@@ -1,21 +1,33 @@
 import * as nodemailer from 'nodemailer';
+import * as fs from 'fs';
 
 let mailingService={}
 
-mailingService.sendEmail = async (userCreds,target,emailText)=>{
+mailingService.sendEmail = async (userCreds,target,emailTextPath)=>{
     let transporter = nodemailer.createTransport({
         service:'gmail',
         auth:userCreds
     })
+    let textRead = await mailingService.getFile(emailTextPath);
+    console.log(textRead)
     let mailOptions = {
         from: userCreds.user,
         to:target,
-        text:emailText
+        text:textRead
     }
     let info = transporter.sendMail(mailOptions)
     if(!info) return next(new Error("Could not send email"))
     return 'Email sent: ' + (await info).response;
 
+}
+
+mailingService.getFile = async(path)=>{
+    try {
+        const data = fs.readFileSync(path, 'utf8');
+        return data;
+      } catch (err) {
+        next(err);
+      }
 }
 
 export const mailService=mailingService;
